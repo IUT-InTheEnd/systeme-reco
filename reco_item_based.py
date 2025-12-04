a85378021841ce7710e961290bcb5290244cb293
stephantoine <stephouille1022@gmail.com>
2025-12-04 14:58:14 +0100

    add basic functions and item-based function

---
+++ reco_item_based.py
@@ -0,0 +1,95 @@
+import math
+
+
+
+# fonctions de base sur les vecteurs
+
+def prodScal(a, b):
+    if len(a) != len(b):
+        raise ValueError("Les vecteurs doivent avoir la même longueur")
+    res = 0
+    for i in range(len(a)):
+        res += a[i] * b[i]
+    return res
+
+def norm(vect):
+    res = 0
+    for x in vect:
+        res += x**2
+    return math.sqrt(res)
+
+def inter(vectA, vectB):
+    if len(vectA) != len(vectB):
+        raise ValueError("Les vecteurs doivent avoir la même longueur")
+    count = 0
+    for i in range(len(vectA)):
+        if ((vectA[i] == vectB[i]) and (vectA[i] != 0)):    
+            count += 1
+    return count
+
+def union(vectA, vectB):
+    if len(vectA) != len(vectB):
+        raise ValueError("Les vecteurs doivent avoir la même longueur")
+    count = 0
+    for i in range(len(vectA)):
+        if (vectA[i] != 0 or vectB[i] != 0):
+            count += 1
+    return count
+
+def distEucl(vectA, vectB):
+    if len(vectA) != len(vectB):
+        raise ValueError("Les vecteurs doivent avoir la même longueur")
+    res = 0
+    for i in range(len(vectA)):
+        res+= (vectA[i] - vectB[i])**2
+    return math.sqrt(res)
+
+
+
+# fonctions de similarité
+
+def simCos(vectA, vectB):
+    normA = norm(vectA)
+    normB = norm(vectB)
+    if normA == 0 or normB == 0:
+        raise ValueError("La norme d'un vecteur ne peut pas être zéro")
+    return prodScal(vectA, vectB) / (normA * normB)
+
+def simEucl1(vectA, vectB):
+    return 1 / (1 + distEucl(vectA, vectB))
+
+def simEucl2(vectA, vectB):
+    return math.exp(-distEucl(vectA, vectB))
+
+def jacc(vectA, vectB):
+    u = union(vectA, vectB)
+    if u == 0:
+        return 0  
+    return inter(vectA, vectB) / u
+
+
+
+# fonction de prédiction item-based 
+
+def itemBased(u, i, matr, items):
+    sNumerateur = 0
+    sDenominateur = 0
+
+    # calcul des similarités entre l'item i et tous les autres items
+    sim = [0] * len(items)
+    for j in range(len(items)):
+        if j != i:
+            sim[j] = simCos(items[i], items[j]) 
+        else:
+            sim[j] = 0
+
+    # calcul de la prédiction
+    for j in range(len(items)):
+        if matr[u][j] != 0:               
+            sNumerateur += matr[u][j] * sim[j] # somme du produit entre la note de u pour chaque item et la similarité de l'item et i
+            sDenominateur += abs(sim[j]) # somme des similarités entre les tous les items et i
+
+    if sDenominateur == 0: # eviter la division par zero
+        return 0  
+
+    return sNumerateur / sDenominateur
