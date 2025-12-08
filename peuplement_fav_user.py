9aa64cce4ac4d4a28b4cd4f11241c9db07e7f07f
PIlouFace <24657574+Pilou26021@users.noreply.github.com>
2025-12-08 12:33:54 +0100

    start peuplement

--- peuplement_fav_user.py
+++ peuplement_fav_user.py
@@ -20,19 +20,54 @@ def main():
     users = cursor.fetchall()
     for user in users:
         user_id = user[0]
-        music_envy_today = user[10]
-        feeling = user[11]
-        music_preference = user[12]
-        music_style_preference = user[13]
-        music_reason = user[14]
-        listening_context = user[15]
-        current_music_type = user[16]
-        attend_live_concert = user[19]
-        explicit_ok = user[21]
-        avg_song_length = user[22]
-        recommanded_artists = user[24]
-        print(user_id, music_envy_today, feeling, music_preference, music_style_preference, music_reason, listening_context, current_music_type, attend_live_concert, explicit_ok, avg_song_length, recommanded_artists)
+        use_age_moyenne = user[1]
+        user_job = user[2]
+        user_plays_music = user[3]
+        user_pseudo = user[4]
+        user_password = user[5]
+        user_sex = user[6]
+        user_instruments = user[7]
+        user_music_contexts = user[8]
+        user_profile_id = user[9]
+        user_profile_id_id = user[10]
+        user_music_envy_today = user[11]
+        user_feeling = user[12]
+        user_music_preference = user[13]
+        user_music_style_preference = user[14]
+        user_music_reason = user[15]
+        user_listening_context = user[16]
+        user_current_music_type = user[17]
+        user_usual_listening_mode = user[18]
+        user_likes_discovery = user[19]
+        user_attend_live_concert = user[20]
+        user_repeat_listening = user[21]
+        user_explicit_ok = user[22]
+        user_avg_song_length = user[23]
+        user_avg_daily_listen_time = user[24]
+        user_recommanded_artists = user[25]
+        
+        # Récupère les genres préférés de l'utilisateur
+        cursor.execute("SELECT genre_id FROM sae5_6.ajoute_genre_favoris WHERE user_id = %s;", (user_id,))
+        fav_genres = cursor.fetchall()
+        fav_genres_ids = []
+        fav_genres_ids = [genre[0] for genre in fav_genres]
 
+            
+        # Récupère les musics qui satisfait le genre favoris, l'explicit ok, la durée moyenne des chansons et un like sur chaque user_recommanded_artists
+        query = """
+                SELECT m.track_id, m.artist_id, m.album_id
+                FROM sae5_6.track m
+                WHERE m.genre_id = ANY(%s)
+                AND (%s = 1 OR m.explicit = 0)
+                AND m.duration <= %s + 30
+                AND m.duration >= %s - 30
+                AND (%s = '{}' OR m.artist_id = ANY(%s))
+                LIMIT 10;
+            """
+        
+        fav_musics = cursor.fetchall()
+        print(fav_musics)
+        ()    
 if __name__ == "__main__":
     main()
     
