0f97d1c966494e947934dd881f2716224ef3a4de
Arcthuruss <73853221+Arcthuruss@users.noreply.github.com>
2025-12-18 13:47:04 +0100

    fin reco p3 + population ecoutes

---
+++ popu_ecoute.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import psycopg2
+import numpy as np
+from math import floor
+
+conn = psycopg2.connect(
+        dbname="InTheEnd_DB",
+        user="InTheEnd_User",
+        password="InTheEnd_Password",
+        host="localhost",
+        port="25000"
+    )
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM sae5_6.ajoute_favori;")
+data = cur.fetchall()
+columns = [desc[0] for desc in cur.description]
+df = pd.DataFrame(data, columns=columns)
+# print(df.head())
+
+insert_sql = "INSERT INTO sae5_6.user_ecoute (user_id, track_id, nb_ecoute) VALUES "
+
+for index, row in df.iterrows() :
+    nb_ecoute = floor(np.random.normal(5000, 2500))
+    while nb_ecoute < 1 :
+        nb_ecoute = floor(np.random.normal(5000, 2500))
+    insert_sql += f"({row['user_id']}, {row['track_id']}, {nb_ecoute}), "
+
+insert_sql = insert_sql[:-2] + ';'
+cur.execute(insert_sql)
+conn.commit()
---
+++ reco_user_based_p3.py
@@ -0,0 +1,63 @@
+from math import log
+import load
+import pandas as pd
+from reco_user_based_p2 import load_user_favorites, similarity_user_favorites
+
+weights = {"tracks": 0.3, "artists": 0.2, "albums": 0.2, "genres": 0.2, "languages": 0.1}
+total_weight = sum(weights.values())
+jaccard = lambda A, B : len(A & B) / len(A | B)
+
+def note(listens, is_track_fav, is_artist_fav, is_album_fav, is_genre_fav, listen_language) :
+    listen_score = (log(listens) + 1)/10 if listens <= 8103 else 1
+    return (
+        listen_score + (
+            is_track_fav * weights["tracks"] +
+            is_artist_fav * weights["artists"] +
+            is_album_fav * weights["albums"] +
+            is_genre_fav * weights["genres"] +
+            listen_language * weights["languages"]
+            ) / total_weight
+        ) / 2
+
+def get_pred_for_user(user_id, track_id, sim=similarity_user_favorites) :
+    conn = load.connection_db()
+    cur = conn.cursor()
+    cur.execute(f"SELECT user_id, nb_ecoute FROM sae5_6.user_ecoute WHERE track_id = {track_id};")
+    data = cur.fetchall()
+    columns = [desc[0] for desc in cur.description]
+    users = pd.DataFrame(data, columns=columns)
+
+    cur.execute(f"SELECT artist_id, album_id, language_id FROM sae5_6.track NATURAL JOIN sae5_6.realiser NATURAL JOIN sae5_6.track_chanter_en WHERE track_id = {track_id};")
+    data = cur.fetchall()[0]
+    cur.execute(f"SELECT genre_id FROM sae5_6.contient_genres WHERE track_id = {track_id};")
+    data = cur.fetchall()
+    track_info = {
+        "artist": data[0],
+        "album": data[1],
+        "genres": [i[0] for i in data],
+        "language": data[2]
+        }
+    user_fav = load_user_favorites(user_id, conn)
+    if all(len(val) == 0 for val in user_fav.values()) :
+        raise Exception("user has no favorites")
+    deno = 0
+    nume = 0
+    for index, row in users.iterrows() :
+        target_user_id = int(row['user_id'])
+        if target_user_id == user_id : continue
+        target_favs = load_user_favorites(target_user_id, conn)
+        n = note(
+            int(row['nb_ecoute']),
+            track_id in target_favs['tracks'],
+            track_info['artist'] in target_favs['artists'],
+            track_info['album'] in target_favs['albums'],
+            jaccard(set(track_info['genres']), set(target_favs['genres'])),
+            track_info['language'] in target_favs['languages']
+            )
+        simmilarity = sim(user_fav, target_favs)
+        deno += n * simmilarity
+        nume += simmilarity
+    return deno / nume
+
+if __name__ == "__main__" :
+    get_pred_for_user(12, 155880)
